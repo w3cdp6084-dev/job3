a6945b98645652127298946376fe234a980fd38d
w3cdp6084-dev <yusukew3@gmail.com>
2023-11-04 11:51:36 +0900

    sidebar分離前

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+// pages/index.tsx
 import { marked } from 'marked';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface MarkdownFile {
   title: string;
@@ -7,22 +8,60 @@ interface MarkdownFile {
   content: string;
 }
 
+interface Category {
+  [key: string]: MarkdownFile[];
+}
+
 // ページコンポーネント
-const HomePage = ({ markdownFiles }: { markdownFiles: MarkdownFile[] }) => {
-  const [selectedSlug, setSelectedSlug] = useState(markdownFiles[0]?.slug);
-  const selectedMarkdownFile = markdownFiles.find(file => file.slug === selectedSlug);
+const HomePage = ({ categories }: { categories: Category }) => {
+  // 最初のカテゴリを取得してデフォルトで展開
+  const firstCategoryName = Object.keys(categories)[0];
+  const [selectedSlug, setSelectedSlug] = useState('');
+  const [openCategories, setOpenCategories] = useState<string[]>([]);
+  useEffect(() => {
+    const startedSlug = categories['apiguide']?.find(file => file.slug.endsWith('started'))?.slug;
+    if (startedSlug) {
+      setSelectedSlug(startedSlug);
+      setOpenCategories(['apiguide']);
+    }
+  }, [categories]);
+  const toggleCategory = (category: string) => {
+    setOpenCategories(openCategories.includes(category)
+      ? openCategories.filter(c => c !== category)
+      : [...openCategories, category]);
+  };
+
+  // 選択されたマークダウンファイルを探す
+  const selectedMarkdownFile = Object.values(categories)
+    .flat()
+    .find(file => file.slug === selectedSlug);
 
   return (
-    <div>
-      <h1>ドキュメント一覧</h1>
-      <ul>
-        {markdownFiles.map((file) => (
-          <li key={file.slug} onClick={() => setSelectedSlug(file.slug)}>
-            {file.title}
-          </li>
+    <div style={{ display: 'flex' }}>
+      <div style={{ marginRight: '20px' }}>
+        <h1>ドキュメント一覧</h1>
+        {Object.entries(categories).map(([category, files]) => (
+          <div key={category}>
+            <button onClick={() => toggleCategory(category)}>
+              {category}
+            </button>
+            {openCategories.includes(category) && (
+              <ul>
+                {files.map((file) => (
+                  <li key={file.slug} onClick={() => setSelectedSlug(file.slug)}>
+                    {file.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         ))}
-      </ul>
-      <div dangerouslySetInnerHTML={{ __html: selectedMarkdownFile?.content ?? 'Content not found' }} />
+      </div>
+      <div>
+        <h2>コンテンツ</h2>
+        {/* 選択されたマークダウンファイルの内容を表示 */}
+        <div dangerouslySetInnerHTML={{ __html: selectedMarkdownFile?.content ?? 'Content not found' }} />
+      </div>
     </div>
   );
 };
@@ -31,35 +70,50 @@ const HomePage = ({ markdownFiles }: { markdownFiles: MarkdownFile[] }) => {
 export async function getStaticProps() {
   const path = require('path');
   const fs = require('fs');
-  const docsDirectory = path.join(process.cwd(), 'docs');
 
-  // 再帰的にディレクトリを読み込むためのヘルパー関数を getStaticProps 内に配置
-  const getAllMarkdownFiles = (dirPath: string, arrayOfFiles: MarkdownFile[] = [], rootPath: string) => {
-    const files = fs.readdirSync(dirPath);
+  // ディレクトリのパスからカテゴリ名を取得する関数
+  const getCategoryName = (dirPath: string, rootPath: string) => {
+    const relativePath = path.relative(rootPath, dirPath);
+    return relativePath.split(path.sep)[0];
+  };
 
-    files.forEach(file => {
-      if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
-        arrayOfFiles = getAllMarkdownFiles(path.join(dirPath, file), arrayOfFiles, rootPath);
-      } else if (file.endsWith('.md')) {
-        const fullPath = path.join(dirPath, file);
+  // 再帰的にディレクトリを読み込むためのヘルパー関数
+  const getAllMarkdownFiles = (dirPath: string, rootPath: string): MarkdownFile[] => {
+    let filesInDirectory: MarkdownFile[] = [];
+    const items = fs.readdirSync(dirPath);
+
+    items.forEach(item => {
+      const fullPath = path.join(dirPath, item);
+      if (fs.statSync(fullPath).isDirectory()) {
+        filesInDirectory = filesInDirectory.concat(getAllMarkdownFiles(fullPath, rootPath));
+      } else if (item.endsWith('.md')) {
         const fileContents = fs.readFileSync(fullPath, 'utf8');
         const titleMatch = fileContents.match(/^# (.*)\n/);
-        const title = titleMatch ? titleMatch[1] : path.basename(file, '.md');
+        const title = titleMatch ? titleMatch[1] : path.basename(item, '.md');
         const content = marked(fileContents);
         const slug = fullPath.replace(rootPath, '').replace(/^\/+/, '').replace(/\.md$/, '');
-        arrayOfFiles.push({ title, slug, content });
+        filesInDirectory.push({ title, slug, content });
       }
     });
 
-    return arrayOfFiles;
+    return filesInDirectory;
   };
 
-  const markdownFiles = getAllMarkdownFiles(docsDirectory, [], docsDirectory);
+  const docsDirectory = path.join(process.cwd(), 'docs');
+  const markdownFiles = getAllMarkdownFiles(docsDirectory, docsDirectory);
+
+  // カテゴリとファイルのリストを作成
+  const categories = markdownFiles.reduce((acc: Category, file) => {
+    const category = getCategoryName(file.slug, '');
+    if (!acc[category]) acc[category] = [];
+    acc[category].push(file);
+    return acc;
+  }, {});
 
   return {
     props: {
-      markdownFiles,
-    },
+      categories
+    }
   };
 }
 
